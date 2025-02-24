0fcb47d702fdf8d1dda9a0365688e22a95a76ddb
gugdun <gugdunoff@gmail.com>
2025-02-24 16:49:04 +0300

    add: functions to add tags, links and data

--- qs/src/parser.c
+++ qs/src/parser.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <memory.h>
 #include <parser.h>
 
 #define LINE_ALLOC_SIZE 32
+#define DATA_CHUNK_SIZE 256
 
 size_t getline(char **lineptr, size_t *n, FILE *stream) {
     char *bufptr = NULL;
@@ -56,7 +59,48 @@ size_t getline(char **lineptr, size_t *n, FILE *stream) {
     return p - bufptr - 1;
 }
 
-int parse_line(int number, char *line)
+void add_tag(char *name, uint16_t addr, object_t *obj)
+{
+    if (!obj->tags) obj->tags = (tag_t *)malloc(sizeof(tag_t));
+    else obj->tags = (tag_t *)realloc(obj->tags, sizeof(tag_t) * (obj->tag_count + 1));
+    tag_t *last_tag = &obj->tags[obj->tag_count - 1];
+    last_tag->addr = addr;
+    last_tag->name_size = (uint8_t)strlen(name);
+    last_tag->name = (char *)malloc(last_tag->name_size);
+    strcpy(last_tag->name, (const char*)name);
+    ++obj->tag_count;
+}
+
+void add_link(char *name, uint16_t addr, object_t *obj)
+{
+    if (!obj->links) obj->links = (link_t *)malloc(sizeof(link_t));
+    else obj->links = (link_t *)realloc(obj->links, sizeof(link_t) * (obj->link_count + 1));
+    link_t *last_link = &obj->links[obj->link_count - 1];
+    last_link->addr = addr;
+    last_link->name_size = (uint8_t)strlen(name);
+    last_link->name = (char *)malloc(last_link->name_size);
+    strcpy(last_link->name, (const char*)name);
+    ++obj->link_count;
+}
+
+void add_data(uint8_t *chunk, uint16_t size, object_t *obj)
+{
+    if (!obj->data)
+    {
+        obj->data = (uint8_t *)malloc(DATA_CHUNK_SIZE);
+        obj->alloc_size = DATA_CHUNK_SIZE;
+        obj->size = 0;
+    }
+    else if (obj->size + size > obj->alloc_size)
+    {
+        obj->data = (uint8_t *)realloc(obj->data, obj->alloc_size + DATA_CHUNK_SIZE);
+        obj->alloc_size += DATA_CHUNK_SIZE;
+    }
+    memcpy((void *)&obj->data[obj->size], (void *)chunk, (size_t)size);
+    obj->size += size;
+}
+
+int parse_line(int number, char *line, object_t *obj)
 {
     printf("%5d | %s", number, line);
     return 0;
@@ -64,8 +108,6 @@ int parse_line(int number, char *line)
 
 int parse_file(char *path, object_t *obj)
 {
-    int code = 0;
-
     FILE *file = fopen(path, "r");
     if (!file)
     {
@@ -73,15 +115,17 @@ int parse_file(char *path, object_t *obj)
         return 1;
     }
 
-    ssize_t read;
+    ssize_t read = 0;
+    int exit_code = 0;
     int number = 1;
     size_t len = LINE_ALLOC_SIZE;
     char *line = (char *)malloc(LINE_ALLOC_SIZE);
+    
     while ((read = getline(&line, &len, file)) != -1)
     {
-        if (parse_line(number, line))
+        if (parse_line(number, line, obj))
         {
-            code = 1;
+            exit_code = 1;
             break;
         }
         ++number;
@@ -90,6 +134,6 @@ int parse_file(char *path, object_t *obj)
     free(line);
     fclose(file);
     
-    return code;
+    return exit_code;
 }
 
