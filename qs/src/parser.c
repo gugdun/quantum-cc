92798a6111c8fcdc34bf6e076e9935a5a236ce40
gugdun <gugdunoff@gmail.com>
2025-02-24 20:51:55 +0300

    add: tag parsing

--- qs/src/parser.c
+++ qs/src/parser.c
@@ -3,11 +3,127 @@
 #include <string.h>
 #include <memory.h>
 #include <parser.h>
+#include <state.h>
 
 #define LINE_ALLOC_SIZE 32
 #define DATA_CHUNK_SIZE 256
+#define DELIMITERS " \n\t,"
 
-size_t getline(char **lineptr, size_t *n, FILE *stream) {
+size_t _getline(char **lineptr, size_t *n, FILE *stream);
+void add_tag(char *name, uint16_t addr, object_t *obj);
+void add_link(char *name, uint16_t addr, object_t *obj);
+void add_data(uint8_t *chunk, uint16_t size, object_t *obj);
+
+int parse_line(int number, char *line, object_t *obj)
+{
+#ifdef DEBUG
+    printf("%5d | %s", number, line);
+#endif
+    state_t *state = get_state();
+    
+    char *cmd = strtok(line, DELIMITERS);
+    if (!cmd || *cmd == ';') return 0;
+    
+    size_t len = strlen(cmd);
+    if (cmd[len - 1] == ':')
+    {
+        add_tag(cmd, state->addr + state->org, obj);
+        return 0;
+    }
+    
+    char *arg1 = strtok(NULL, DELIMITERS);
+    char *arg2 = strtok(NULL, DELIMITERS);
+    char *arg3 = strtok(NULL, DELIMITERS);
+
+#ifdef DEBUG
+    printf("TOKENS| cmd: %s, arg1: %s, arg2: %s, arg3: %s\n", cmd, arg1, arg2, arg3);
+#endif
+    
+    return 0;
+}
+
+int parse_file(char *path, object_t *obj)
+{
+    FILE *file = fopen(path, "r");
+    if (!file)
+    {
+        printf("File not found: %s\n", path);
+        return 1;
+    }
+
+    ssize_t read = 0;
+    int exit_code = 0;
+    int number = 1;
+    size_t len = LINE_ALLOC_SIZE;
+    char *line = (char *)malloc(LINE_ALLOC_SIZE);
+    
+    while ((read = _getline(&line, &len, file)) != -1)
+    {
+        if (parse_line(number, line, obj))
+        {
+            exit_code = 1;
+            break;
+        }
+        ++number;
+    }
+
+    free(line);
+    fclose(file);
+    
+    return exit_code;
+}
+
+void add_tag(char *name, uint16_t addr, object_t *obj)
+{
+    if (!obj->tags) obj->tags = (tag_t *)malloc(sizeof(tag_t));
+    else obj->tags = (tag_t *)realloc(obj->tags, sizeof(tag_t) * (obj->tag_count + 1));
+    tag_t *last_tag = &obj->tags[obj->tag_count];
+    last_tag->addr = addr;
+    last_tag->name_size = (uint8_t)strlen(name);
+    last_tag->name = (char *)malloc(last_tag->name_size);
+    strcpy(last_tag->name, (const char*)name);
+    ++obj->tag_count;
+#ifdef DEBUG
+    printf("FNCALL| add_tag(%s, 0x%04X)\n", last_tag->name, last_tag->addr);
+#endif
+}
+
+void add_link(char *name, uint16_t addr, object_t *obj)
+{
+    if (!obj->links) obj->links = (link_t *)malloc(sizeof(link_t));
+    else obj->links = (link_t *)realloc(obj->links, sizeof(link_t) * (obj->link_count + 1));
+    link_t *last_link = &obj->links[obj->link_count];
+    last_link->addr = addr;
+    last_link->name_size = (uint8_t)strlen(name);
+    last_link->name = (char *)malloc(last_link->name_size);
+    strcpy(last_link->name, (const char*)name);
+    ++obj->link_count;
+#ifdef DEBUG
+    printf("FNCALL| add_link(%s, 0x%04X)\n", last_link->name, last_link->addr);
+#endif
+}
+
+void add_data(uint8_t *chunk, uint16_t size, object_t *obj)
+{
+    if (!obj->data)
+    {
+        obj->data = (uint8_t *)malloc(DATA_CHUNK_SIZE);
+        obj->alloc_size = DATA_CHUNK_SIZE;
+        obj->size = 0;
+    }
+    else if (obj->size + size > obj->alloc_size)
+    {
+        obj->data = (uint8_t *)realloc(obj->data, obj->alloc_size + DATA_CHUNK_SIZE);
+        obj->alloc_size += DATA_CHUNK_SIZE;
+    }
+    memcpy((void *)&obj->data[obj->size], (void *)chunk, (size_t)size);
+    obj->size += size;
+#ifdef DEBUG
+    printf("FNCALL| add_data(0x%04X)\n", size);
+#endif
+}
+
+size_t _getline(char **lineptr, size_t *n, FILE *stream) {
     char *bufptr = NULL;
     char *p = bufptr;
     size_t size;
@@ -58,82 +174,3 @@ size_t getline(char **lineptr, size_t *n, FILE *stream) {
 
     return p - bufptr - 1;
 }
-
-void add_tag(char *name, uint16_t addr, object_t *obj)
-{
-    if (!obj->tags) obj->tags = (tag_t *)malloc(sizeof(tag_t));
-    else obj->tags = (tag_t *)realloc(obj->tags, sizeof(tag_t) * (obj->tag_count + 1));
-    tag_t *last_tag = &obj->tags[obj->tag_count - 1];
-    last_tag->addr = addr;
-    last_tag->name_size = (uint8_t)strlen(name);
-    last_tag->name = (char *)malloc(last_tag->name_size);
-    strcpy(last_tag->name, (const char*)name);
-    ++obj->tag_count;
-}
-
-void add_link(char *name, uint16_t addr, object_t *obj)
-{
-    if (!obj->links) obj->links = (link_t *)malloc(sizeof(link_t));
-    else obj->links = (link_t *)realloc(obj->links, sizeof(link_t) * (obj->link_count + 1));
-    link_t *last_link = &obj->links[obj->link_count - 1];
-    last_link->addr = addr;
-    last_link->name_size = (uint8_t)strlen(name);
-    last_link->name = (char *)malloc(last_link->name_size);
-    strcpy(last_link->name, (const char*)name);
-    ++obj->link_count;
-}
-
-void add_data(uint8_t *chunk, uint16_t size, object_t *obj)
-{
-    if (!obj->data)
-    {
-        obj->data = (uint8_t *)malloc(DATA_CHUNK_SIZE);
-        obj->alloc_size = DATA_CHUNK_SIZE;
-        obj->size = 0;
-    }
-    else if (obj->size + size > obj->alloc_size)
-    {
-        obj->data = (uint8_t *)realloc(obj->data, obj->alloc_size + DATA_CHUNK_SIZE);
-        obj->alloc_size += DATA_CHUNK_SIZE;
-    }
-    memcpy((void *)&obj->data[obj->size], (void *)chunk, (size_t)size);
-    obj->size += size;
-}
-
-int parse_line(int number, char *line, object_t *obj)
-{
-    printf("%5d | %s", number, line);
-    return 0;
-}
-
-int parse_file(char *path, object_t *obj)
-{
-    FILE *file = fopen(path, "r");
-    if (!file)
-    {
-        printf("File not found: %s\n", path);
-        return 1;
-    }
-
-    ssize_t read = 0;
-    int exit_code = 0;
-    int number = 1;
-    size_t len = LINE_ALLOC_SIZE;
-    char *line = (char *)malloc(LINE_ALLOC_SIZE);
-    
-    while ((read = getline(&line, &len, file)) != -1)
-    {
-        if (parse_line(number, line, obj))
-        {
-            exit_code = 1;
-            break;
-        }
-        ++number;
-    }
-
-    free(line);
-    fclose(file);
-    
-    return exit_code;
-}
-
